dbd173804af024ea2094d7741fd9f60bbe6de6b7
omgitzjoe <omgitzjoetolley@gmail.com>
2025-07-15 13:48:27 -0600

    chngs

--- incomeOptimizer/incomeOptimizer.js
+++ incomeOptimizer/incomeOptimizer.js
@@ -1,13 +1,19 @@
 function calcMileage(){
-    let totalMiles=document.getElementById('totalMiles').value;
-    let workMiles=document.getElementById('workMiles').value;
-    let expenses=document.getElementById('estimate').value;
-    let percentage=workMiles/totalMiles;
+    let totalMiles=parseFloat(document.getElementById('totalMiles').value);
+    let workMiles=parseFloat(document.getElementById('workMiles').value);
+    let expenses=parseFloat(document.getElementById('estimate').value);
+    let percentage=workMiles/totalMiles;    
     let standard = workMiles*.7;
-    document.getElementById('resultS').innerHTML ='$' + standard.toFixed(2);
-    let actual= percentage*expenses;
-    document.getElementById('resultA').innerHTML ='$' + actual.toFixed(2);
-    document.getElementById('workPercent').innerHTML=percentage*100+'%';
+    if (totalMiles>=workMiles){
+        document.getElementById('resultS').innerHTML ='$' + standard.toFixed(2);
+        let actual= percentage*expenses;
+        document.getElementById('resultA').innerHTML ='$' + actual.toFixed(2);
+        document.getElementById('workPercent').innerHTML=percentage*100+'%';
+    } else{
+        document.getElementById('resultS').innerHTML='ERROR'
+        document.getElementById('resultA').innerHTML='CHECK'
+        document.getElementById('workPercent').innerHTML='MILES'
+    }
 }
 
 function calcIncome(){
@@ -20,5 +26,9 @@ function calcIncome(){
     let deductible=(vehExpenses*usePercent)+otherExp;
     let income=payIncome+seIncome+otherIncome;
     let actualEarnings=income-deductible;
+    if (actualEarnings>0){
     document.getElementById('resultINC').innerHTML='$' + actualEarnings.toFixed(2);
+    } else {
+        document.getElementById('resultINC').innerHTML='0.00';
+    }
 }
